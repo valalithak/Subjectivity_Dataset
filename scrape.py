b1b63779f45fbdeb664fa37e14a91a4815779683
valalithak <v.a.lalitha@research.iiit.ac.in>
2019-05-22 16:11:22 +0530

    Articles added

--- scrape.py
+++ scrape.py
@@ -3,30 +3,13 @@ import requests
 import datetime
 from time import sleep
 import os
-
-url = 'https://timesofindia.indiatimes.com/india/kerala-flood-toll-rises-to-29-red-alert-in-8-districts/articleshow/65360414.cms'
+# f = open('deepika_ranveer_ht.txt', 'w')
+url = 'https://www.indiatoday.in/deepika-ranveer-wedding?page=2'
 r = requests.get(url)
 data = r.text
 soup = BeautifulSoup(data, "lxml")
-headline = soup.find('div', attrs = {'class': '_38kVl'})
-# print(headline)
-date = headline.find('div', attrs = {'class': '_3Mkg- byline'})
-for text in date:
-    print(text)
-headline2 = headline.find('h1')
-for text in headline2:
-    print(text)
 
-a = soup.find('div', attrs = {'class':'_3WlLe clearfix '})
-
-# print(a)
-s = ""
-for text in a:
-    if str(text) == "<br/>":
-        continue
-    elif str(text).find("<") != -1:
-        continue
-    else:
-      s += str(text)
-
-print(s) 
+b = soup.findAll('h2')
+for link in b:
+    c = link.find('a').get('href')
+    print("https://www.indiatoday.in" + str(c))
