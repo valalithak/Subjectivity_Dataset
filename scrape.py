818c266362452472c77ebe139b0ace22eefa5587
valalithak <v.a.lalitha@research.iiit.ac.in>
2019-05-23 14:57:49 +0530

    Articles added

--- scrape.py
+++ scrape.py
@@ -4,12 +4,12 @@ import datetime
 from time import sleep
 import os
 # f = open('deepika_ranveer_ht.txt', 'w')
-url = 'https://www.hindustantimes.com/topic/maharashtra-plastic-ban'
+url = 'https://www.thehindu.com/search/?q=karnataka%20elections%202018&order=DESC&sort=score&page=4'
 r = requests.get(url)
 data = r.text
 soup = BeautifulSoup(data, "lxml")
 
-b = soup.findAll('div', attrs = {'class':'authorListing'})
+b = soup.findAll('div', attrs = {'class':'story-card story-card75x1-cont'})
 for link in b:
     c = link.find('a').get('href')
     print(c)
