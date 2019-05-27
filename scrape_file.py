16bc4af3e15319f689c0cfd1ff2a40db68587da2
valalithak <v.a.lalitha@research.iiit.ac.in>
2019-05-27 16:45:40 +0530

    HT added

--- scrape_file.py
+++ scrape_file.py
@@ -120,7 +120,35 @@ def indianexpress():
                 sleep(2)
                 
                    
+def ht():
+    nyt_paths = commands.getoutput("find -name *ht.txt").split('\n')
+    for path in nyt_paths:
+        f = open(path, 'r')
+        urls = f.readlines()
+        f.close()
+        for idx, url in enumerate(urls):
+            if url != '\n':
+                print url.strip()
+                r = requests.get(url.strip())
+                data = r.text
+                soup = BeautifulSoup(data, "lxml")
+                headline = soup.findAll('h1')[0].text
+                print headline
+                paras = soup.findAll('p')
                 
+                content = " "
+                for i in range(0, len(paras)-1):
+                    article = paras[i].text
+                    content += article + "\n"
+
+                article = content
+                new_path = ('/').join(path.split('/')[:-1]) + '/ht_article_' + str(idx + 1) + '.txt'
+                f = open(new_path, 'w')
+                f.write(headline.encode('utf-8') + '\n')
+                f.write(article.encode('utf-8'))
+                f.close()
+                sleep(2)
+              
                 
                 
                
@@ -128,5 +156,6 @@ def indianexpress():
 #nyt()
 # et()
 # hindu()
-indianexpress()
+# indianexpress()
+ht()
 
