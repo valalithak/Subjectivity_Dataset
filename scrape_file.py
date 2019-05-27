92f2865c127744411cbc06b1737bd24e56201d90
valalithak <v.a.lalitha@research.iiit.ac.in>
2019-05-27 16:58:49 +0530

    BBC added

--- scrape_file.py
+++ scrape_file.py
@@ -149,7 +149,36 @@ def ht():
                 f.close()
                 sleep(2)
               
+
+def bbc():
+    nyt_paths = commands.getoutput("find -name *bbc.txt").split('\n')
+    for path in nyt_paths:
+        f = open(path, 'r')
+        urls = f.readlines()
+        f.close()
+        for idx, url in enumerate(urls):
+            if url != '\n':
+                print url.strip()
+                r = requests.get(url.strip())
+                data = r.text
+                soup = BeautifulSoup(data, "lxml")
+                headline = soup.findAll('h1')[0].text
+                print headline
+                paras = soup.findAll('p')
                 
+                content = " "
+                for i in range(11, len(paras)-3):
+                    article = paras[i].text
+                    content += article + "\n"
+                
+                # print content
+                article = content
+                new_path = ('/').join(path.split('/')[:-1]) + '/ht_article_' + str(idx + 1) + '.txt'
+                f = open(new_path, 'w')
+                f.write(headline.encode('utf-8') + '\n')
+                f.write(article.encode('utf-8'))
+                f.close()
+                sleep(2)            
                 
                
 #indiat()
@@ -157,5 +186,6 @@ def ht():
 # et()
 # hindu()
 # indianexpress()
-ht()
+# ht()
+bbc()
 
